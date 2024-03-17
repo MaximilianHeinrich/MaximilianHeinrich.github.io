14591f2556e093976c87e464f51af5cf22bd543a
mcshept <maxhei.21@gmail.com>
2024-03-17 14:06:05 +0100

    Updated resume

---
+++ build/_app/immutable/chunks/stores.3bf1a6ae.js
@@ -0,0 +1,1 @@
+import{d as e}from"./singletons.510a41a8.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
