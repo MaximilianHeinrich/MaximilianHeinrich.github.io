64ed5235faf072c755d7f446994f381b0148ede1
mcshept <maxhei.21@gmail.com>
2024-05-19 14:48:18 +0200

    update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const base = '/slick-portfolio-svelte';
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,18 +13,11 @@ const config = {
 			showToggleButton: 'always'
 		}
 	},
+
 	kit: {
 		adapter: adapter({ fallback: '404.html' }),
-		alias: {
-			$lib: './src/lib',
-			'@data': './src/lib/data',
-			'@components': './src/lib/components',
-			'@md': './src/lib/md',
-			'@stores': './src/lib/stores',
-			'@utils': './src/lib/utils'
-		},
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? base : ''
+			base: dev ? '' : process.env.BASE_PATH,
 		}
 	}
 };
