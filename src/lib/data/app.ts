a92266fec8d4b032526a8910e7534495399b4a31
MaximilianHeinrich <email@maximilian.software>
2026-06-06 12:21:10 +0200

    Cleanup code

--- src/lib/data/app.ts
+++ src/lib/data/app.ts
@@ -1,6 +1,1 @@
 export const titleSuffix = 'Maximilian Andreas Heinrich';
-
-export const title = 'Maximilian Heinrich';
-export const description = 'Developer portfolio of Maximilian Heinrich.';
-export const url = 'https://maximilianhenrich.github.io';
-export const ogImage = `${url}/logos/logo.png`; // you already have this file
